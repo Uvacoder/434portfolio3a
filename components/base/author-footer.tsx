5f9aec452f6a36a0943604a77e4b3e4f2545ed3f
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-08-20 11:59:00 -0400

    Update author-footer.tsx

--- components/base/author-footer.tsx
+++ components/base/author-footer.tsx
@@ -5,14 +5,14 @@ export const AuthorFooter = () => (
   <div className="flex space-x-3 items-center py-6 my-2 border-t">
     <ProfileImage size={56} />
     <div>
-      <div className="font-semibold">Timo Lins</div>
-      <div>Developer + Designer from 🇦🇹</div>
+      <div className="font-semibold">uvacoder</div>
+      <div>Developer + Designer</div>
       <div>
         <a
           className="inline-flex text-gray-500 transition-colors duration-200 hover:text-gray-600"
-          href="https://twitter.com/timolins"
+          href="https://twitter.com/AlecCam43544378"
         >
-          <span>@timolins</span>
+          <span>@AlecCam43544378</span>
         </a>
       </div>
     </div>
