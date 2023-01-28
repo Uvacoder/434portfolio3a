b056e5a2fef38162cfe16489f57dc11af8b7a446
uvacoder <uva22940coder@gmail.com>
2023-01-27 23:45:40 -0500

    Update footer.tsx

--- components/sections/footer.tsx
+++ components/sections/footer.tsx
@@ -2,7 +2,7 @@ export const Footer: React.FC = () => {
   return (
     <div className="text-center text-gray-600 text-sm p-4 mt-8">
       © {new Date().getFullYear()} uvacoder ·{" "}
-      <a className="text-blue-500" href="https://github.com/uvacoder/abcport15">
+      <a className="text-blue-500" href="https://github.com/uvacoder/uvacoder434">
         Source
       </a>
     </div>
